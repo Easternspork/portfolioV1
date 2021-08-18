3ad3140841be14495680115215fdf93fba92039b
easternspork <fumi.okutsu@gmail.com>
2021-08-17 18:42:22 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var app = document.getElementById('app')
+
+new Typewriter(app, {
+  strings: ['programmer', 'web developer', 'gamer B)', 'math enthusiast', 'student', 'human', 'hello world'],
+  autoStart: true,
+  loop: true,
+});
